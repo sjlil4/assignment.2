3e26928bafe6d36e96067007f5bb8cf6ac3633ec
Sam Lillecrapp <sjlil4@student.monash.edu>
2016-10-04 21:25:11 +1100

    added centering on our position

--- assignment2/code/js/recordRegionPage.js
+++ assignment2/code/js/recordRegionPage.js
@@ -1,13 +1,38 @@
 // Code for the Record Region page.
-
+var pos = {lat:3, lng:0}
+ if (navigator.geolocation)
+ {console.log('A')
+   navigator.geolocation.getCurrentPosition (function (location){
+console.log('B')
+         pos.lat = location.coords.latitude ;
+         pos.lng = location.coords.longitude;
+         console.log (pos.lat)
+   })
+ }
+ else alert ("Your device is not suporting geolocation, this is required for this app. Please check settings");
 
 function initialisemap()
 {
-var map
-var mapsettings = {
-  center: {lat:-20, lng: -17},
-  zoom: 2
-}
 
-map = new google.maps.Map(document.getElementById("map"), mapsettings);
+//  var pos;
+
+//  function setcenter(position)
+  //{
+    //  pos =
+    //  {
+    //    lat : position.coords.latitude ,
+    //    lng : position.coords.longitude
+    //  };
+  //  console.log(pos);
+//  }
+
+//  console.log(pos);
+
+  var map;
+  var mapsettings =
+  {
+    center:  pos,
+    zoom: 8
+  };
+  map = new google.maps.Map(document.getElementById("map"), mapsettings);
 }
